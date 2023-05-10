802e6614288f8957ecc9e6b722b335edbcdd20a2
danhhohoang <86829567+danhhohoang@users.noreply.github.com>
2023-05-10 16:58:40 +0700

    ProductAdmin

--- database/migrations/2023_05_10_071941_create_products.php
+++ database/migrations/2023_05_10_071941_create_products.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProducts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('product', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
             $table->bigInteger('type_id');
@@ -39,6 +39,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product');
+        Schema::dropIfExists('products');
     }
-};
+}
